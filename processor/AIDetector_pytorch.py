eb5174dfdd960a134b0af6a7f8e018ad04762374
sHyours <jie_boky@yeah.net>
2021-08-07 08:49:39 +0900

    change for myself action

--- processor/AIDetector_pytorch.py
+++ processor/AIDetector_pytorch.py
@@ -1,93 +1,82 @@
 import torch
 import numpy as np
 from models.experimental import attempt_load
-from utils.general import non_max_suppression, scale_coords, letterbox
+from utils.general import non_max_suppression, scale_coords, xyxy2xywh
+from utils.augmentations import letterbox
 from utils.torch_utils import select_device
-import cv2
+from utils.datasets import LoadImages
 from random import randint
+import cv2
 
 
 class Detector(object):
 
     def __init__(self):
-        self.img_size = 640
-        self.threshold = 0.4
+        self.imgsz = 640
         self.max_frame = 160
+        self.conf_thres = 0.25  # confidence threshold
+        self.iou_thres = 0.45  # NMS IOU threshold
         self.init_model()
 
     def init_model(self):
 
         self.weights = 'weights/final.pt'
-        self.device = '0' if torch.cuda.is_available() else 'cpu'
+        self.device = 'cpu'
         self.device = select_device(self.device)
         model = attempt_load(self.weights, map_location=self.device)
-        model.to(self.device).eval()
-        model.half()
         # torch.save(model, 'test.pt')
-        self.m = model
+        self.model = model
         self.names = model.module.names if hasattr(
             model, 'module') else model.names
         self.colors = [
             (randint(0, 255), randint(0, 255), randint(0, 255)) for _ in self.names
         ]
+        self.stride = int(model.stride.max())  # model stride
 
-    def preprocess(self, img):
+    def detect(self, imageBuffer):
+        # Dataloader
+        lines = []
 
-        img0 = img.copy()
-        img = letterbox(img, new_shape=self.img_size)[0]
-        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img0s = cv2.imdecode(np.frombuffer(imageBuffer, np.uint8), cv2.IMREAD_COLOR)  # BGR
+        # Padded resize
+        img = letterbox(img0s, self.imgsz, stride=self.stride)[0]
+        # Convert
+        img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
         img = np.ascontiguousarray(img)
         img = torch.from_numpy(img).to(self.device)
-        img = img.half()  # 半精度
-        img /= 255.0  # 图像归一化
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+        img = img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if len(img.shape) == 3:
+            img = img[None]  # expand for batch dim
 
-        return img0, img
+        # Inference
 
-    def plot_bboxes(self, image, bboxes, line_thickness=None):
-        tl = line_thickness or round(
-            0.002 * (image.shape[0] + image.shape[1]) / 2) + 1  # line/font thickness
-        for (x1, y1, x2, y2, cls_id, conf) in bboxes:
-            color = self.colors[self.names.index(cls_id)]
-            c1, c2 = (x1, y1), (x2, y2)
-            cv2.rectangle(image, c1, c2, color,
-                          thickness=tl, lineType=cv2.LINE_AA)
-            tf = max(tl - 1, 1)  # font thickness
-            t_size = cv2.getTextSize(
-                cls_id, 0, fontScale=tl / 3, thickness=tf)[0]
-            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-            cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
-            cv2.putText(image, '{} ID-{:.2f}'.format(cls_id, conf), (c1[0], c1[1] - 2), 0, tl / 3,
-                        [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-        return image
-
-    def detect(self, im):
-
-        im0, img = self.preprocess(im)
-
-        pred = self.m(img, augment=False)[0]
-        pred = pred.float()
-        pred = non_max_suppression(pred, self.threshold, 0.3)
-
-        pred_boxes = []
-        image_info = {}
-        count = 0
-        for det in pred:
-            if det is not None and len(det):
+        pred = self.model(img)[0]
+        # NMS
+        pred = non_max_suppression(
+            pred, self.conf_thres, self.iou_thres)
+        # Process predictions
+        for det in pred:  # detections per image
+            im0 = img0s.copy()
+            # normalization gain whwh
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+            if len(det):
+                # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(
                     img.shape[2:], det[:, :4], im0.shape).round()
 
-                for *x, conf, cls_id in det:
-                    lbl = self.names[int(cls_id)]
-                    x1, y1 = int(x[0]), int(x[1])
-                    x2, y2 = int(x[2]), int(x[3])
-                    pred_boxes.append(
-                        (x1, y1, x2, y2, lbl, conf))
-                    count += 1
-                    key = '{}-{:02}'.format(lbl, count)
-                    image_info[key] = ['{}×{}'.format(
-                        x2-x1, y2-y1), np.round(float(conf), 3)]
-
-        im = self.plot_bboxes(im, pred_boxes)
-        return im, image_info
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    clsn = self.names[int(cls)]
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)
+                                        ) / gn).view(-1).tolist()  # normalized xywh
+                    # line = (clsn, cls, *xywh)  # label format
+                    line = (clsn,xywh[0])
+                    print(line)
+                    lines.append(line)
+        lines.sort(key=lambda l: l[0])
+        print(lines)
+        info = []
+        for line in lines:
+            info.append(line[0])
+        return info
